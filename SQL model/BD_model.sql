3082cae0350d7639b88ec338f0202a19a5616555
CezarZbughin <95832576+CezarZbughin@users.noreply.github.com>
2022-01-11 23:07:59 +0200

    n

--- SQL model/BD_model.sql
+++ SQL model/BD_model.sql
@@ -1,7 +1,7 @@
-call professor_asign_subject(2,4);
-call professor_asign_subject(4,4);
-call student_join_subject(1,4,2);
-call student_join_subject(3,4,4);
-call student_join_subject(5,4,4);
-call student_join_subject(6,4,4);
-call subject_get_professor_id_with_min_students(4);
+call schedule_activities(4,1,"MONDAY",8,2,"TUESDAY",10,2,"FRIDAY",16,4,150);
+
+select s.id, s.name, s.description, s.has_lecture, s.has_seminar, s.has_lab, s.date_start, s.date_end,
+al1.students_capacity, al1.lecture_weight, al1.seminar_weight,
+al1.lab_weight, al1.schedule_seminar_day, al1.schedule_lecture_day, al1.schedule_lab_day,
+al1.schedule_seminar_hour, al1.schedule_lecture_hour, al1.schedule_lab_hour, al1.finished_schedule
+from (select  * from teaching t where t.professor_id = 2) as al1 join subject s on al1.subject_id = s.id;
--- StudyingPlatform/src/main/java/com/StudyingPlatform/application/StudyingApplication.java
+++ StudyingPlatform/src/main/java/com/StudyingPlatform/application/StudyingApplication.java
@@ -30,7 +30,7 @@ public class StudyingApplication extends Application {
         }catch(UserNotFoundException e){
             e.printStackTrace();
         }
-        StudyingApplication.jumpToView("log-in-view.fxml");
+        StudyingApplication.jumpToView("admin-view.fxml");
         stage.show();
     }
 
